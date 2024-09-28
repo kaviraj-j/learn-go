e1eaeaa5e2cdc47d28a954fdaf4f92bfccd381b3
Kaviraj Jagadeesan <kavirajmailz@gmail.com>
2024-09-28 18:14:58 +0530

    declaring variables and printing them

---
+++ 04-fundamentals-of-go/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Variable Declaration
+	var favouriteNumber = 2
+	var myName = "Kaviraj"
+	var universityCgpa = 8.8
+	fmt.Println(favouriteNumber, myName, universityCgpa)
+}
