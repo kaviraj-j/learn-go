1ef38c7bec6c556c8434b7f0909868a388b10ed6
Kaviraj Jagadeesan <kavirajmailz@gmail.com>
2024-09-28 18:17:59 +0530

    Declaring variables using short declaration operator

--- 04-fundamentals-of-go/main.go
+++ 04-fundamentals-of-go/main.go
@@ -5,8 +5,14 @@ import "fmt"
 func main() {
 
 	// Variable Declaration
-	var favouriteNumber = 2
-	var myName = "Kaviraj"
-	var universityCgpa = 8.8
+	// var favouriteNumber = 2
+	// var myName = "Kaviraj"
+	// var universityCgpa = 8.8
+	// fmt.Println(favouriteNumber, myName, universityCgpa)
+
+	// Declaring variables using short declaration operator
+	favouriteNumber := 2
+	myName, universityCgpa := "Kaviraj", 8.8
 	fmt.Println(favouriteNumber, myName, universityCgpa)
+
 }
