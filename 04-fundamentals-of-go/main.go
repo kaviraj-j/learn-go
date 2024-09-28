7584539460458b66aa84a3766242362f62ecd657
Kaviraj Jagadeesan <kavirajmailz@gmail.com>
2024-09-28 18:48:25 +0530

    conversion in go

--- 04-fundamentals-of-go/main.go
+++ 04-fundamentals-of-go/main.go
@@ -11,8 +11,19 @@ func main() {
 	// fmt.Println(favouriteNumber, myName, universityCgpa)
 
 	// Declaring variables using short declaration operator
+	/*
+	 */
+
 	favouriteNumber := 2
 	myName, _, universityCgpa := "Kaviraj", 0, 8.8
 	fmt.Println(favouriteNumber, myName, universityCgpa)
+
+	fmt.Println("************** Conversion In go **************")
+	intNum := 30
+	floatNum := 23.92232
+	fmt.Printf("%v of type %T \n", intNum, intNum)
+	fmt.Printf("%v of type %T \n", floatNum, floatNum)
+	intNum = int(floatNum)
+	fmt.Println(intNum)
 
 }
